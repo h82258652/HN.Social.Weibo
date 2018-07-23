27f574c2e78ebef1d6ce5170896b30359ada0f7b
h82258652 <h82258652@hotmail.com>
2018-07-23 19:07:49 +0800

    update

--- HN.Social.Weibo/HN.Social.Weibo.Core/Models/Geo.cs
+++ HN.Social.Weibo/HN.Social.Weibo.Core/Models/Geo.cs
@@ -1,6 +1,13 @@
-﻿namespace HN.Social.Weibo.Models
+﻿using Newtonsoft.Json;
+
+namespace HN.Social.Weibo.Models
 {
     public class Geo
     {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        
+        [JsonProperty("coordinates")]
+        public double[] Coordinates { get; set; }
     }
 }
