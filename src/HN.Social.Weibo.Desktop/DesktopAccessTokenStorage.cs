0cb9ce53e198737ca8f05dfe1534681046c056db
h82258652 <h82258652@hotmail.com>
2019-11-01 17:36:05 +0800

    desktop secure save token

--- src/HN.Social.Weibo.Desktop/DesktopAccessTokenStorage.cs
+++ src/HN.Social.Weibo.Desktop/DesktopAccessTokenStorage.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 using HN.Social.Weibo.Models;
 using Plugin.Settings;
 using Plugin.Settings.Abstractions;
@@ -7,28 +9,42 @@ namespace HN.Social.Weibo
 {
     public class DesktopAccessTokenStorage : IAccessTokenStorage
     {
+        private const string AccessTokenKey = "AccessToken";
+        private const string ExpiresAtKey = "ExpiresAt";
+        private const string UserIdKey = "UserId";
         private const string WeiboFileName = "weibo";
         private static readonly ISettings Settings = CrossSettings.Current;
 
         public void Clear()
         {
-            Settings.Remove("ExpiresAt", WeiboFileName);
-            Settings.Remove("UserId", WeiboFileName);
-            Settings.Remove("AccessToken", WeiboFileName);
+            Settings.Remove(ExpiresAtKey, WeiboFileName);
+            Settings.Remove(UserIdKey, WeiboFileName);
+            Settings.Remove(AccessTokenKey, WeiboFileName);
         }
 
         public AccessToken Load()
         {
-            var expiresAt = Settings.GetValueOrDefault("ExpiresAt", DateTime.MinValue, WeiboFileName);
-            var userId = Settings.GetValueOrDefault("UserId", 0L, WeiboFileName);
-            var value = Settings.GetValueOrDefault("AccessToken", null, WeiboFileName);
-
-            return new AccessToken
+            try
             {
-                ExpiresAt = expiresAt,
-                UserId = userId,
-                Value = value
-            };
+                var protectedExpiresAt = Settings.GetValueOrDefault(ExpiresAtKey, null, WeiboFileName);
+                var protectedUserId = Settings.GetValueOrDefault(UserIdKey, null, WeiboFileName);
+                var protectedValue = Settings.GetValueOrDefault(AccessTokenKey, null, WeiboFileName);
+
+                var expiresAt = DateTime.Parse(Unprotect(protectedExpiresAt));
+                var userId = long.Parse(Unprotect(protectedUserId));
+                var value = Unprotect(protectedValue);
+
+                return new AccessToken
+                {
+                    ExpiresAt = expiresAt,
+                    UserId = userId,
+                    Value = value
+                };
+            }
+            catch
+            {
+                return null;
+            }
         }
 
         public void Save(AccessToken accessToken)
@@ -38,9 +54,27 @@ namespace HN.Social.Weibo
                 throw new ArgumentNullException(nameof(accessToken));
             }
 
-            Settings.AddOrUpdateValue("ExpiresAt", accessToken.ExpiresAt, WeiboFileName);
-            Settings.AddOrUpdateValue("UserId", accessToken.UserId, WeiboFileName);
-            Settings.AddOrUpdateValue("AccessToken", accessToken.Value, WeiboFileName);
+            var protectedValue = Protect(accessToken.Value);
+            var protectedExpiresAt = Protect(accessToken.ExpiresAt.ToString("O"));
+            var protectedUserId = Protect(accessToken.UserId.ToString());
+
+            Settings.AddOrUpdateValue(ExpiresAtKey, protectedExpiresAt, WeiboFileName);
+            Settings.AddOrUpdateValue(UserIdKey, protectedUserId, WeiboFileName);
+            Settings.AddOrUpdateValue(AccessTokenKey, protectedValue, WeiboFileName);
+        }
+
+        private static string Protect(string plaintext)
+        {
+            var bytes = Encoding.UTF8.GetBytes(plaintext);
+            var protectedData = ProtectedData.Protect(bytes, null, DataProtectionScope.CurrentUser);
+            return Convert.ToBase64String(protectedData);
+        }
+
+        private static string Unprotect(string protectedData)
+        {
+            var bytes = Convert.FromBase64String(protectedData);
+            var unprotectedData = ProtectedData.Unprotect(bytes, null, DataProtectionScope.CurrentUser);
+            return Encoding.UTF8.GetString(unprotectedData);
         }
     }
 }
