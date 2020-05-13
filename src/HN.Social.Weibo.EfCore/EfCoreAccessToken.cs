cadcba6496bbe0ddd28fe0ab2c5afd65da3302bc
h82258652 <842053625@qq.com>
2020-05-13 13:38:20 +0800

    add ef core accesstoken storage project

---
+++ src/HN.Social.Weibo.EfCore/EfCoreAccessToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HN.Social.Weibo
+{
+    public class EfCoreAccessToken
+    {
+        public DateTime ExpiresAt { get; set; }
+        
+        public Guid Id { get; set; }
+        
+        public string OwnerAppKey { get; set; }
+
+        public long UserId { get; set; }
+
+        public string Value { get; set; }
+    }
+}
