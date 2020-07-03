6cd45d32166f2369193e6dfb16d0eb72731dd4d8
h82258652 <842053625@qq.com>
2020-07-03 09:24:37 +0800

    update

--- src/HN.Social.Weibo.Core/Models/AuthorizeResult.cs
+++ src/HN.Social.Weibo.Core/Models/AuthorizeResult.cs
@@ -11,7 +11,7 @@ namespace HN.Social.Weibo.Models
         /// 用户授权的唯一票据，用于调用微博的开放接口，同时也是第三方应用验证微博用户登录的唯一票据，第三方应用应该用该票据和自己应用内的用户建立唯一影射关系，来识别登录状态，不能使用本返回值里的UID字段来做登录识别。
         /// </summary>
         [JsonPropertyName("access_token")]
-        public string AccessToken { get; set; }
+        public string AccessToken { get; set; } = default!;
 
         /// <summary>
         /// access_token的生命周期，单位是秒数。
@@ -23,6 +23,6 @@ namespace HN.Social.Weibo.Models
         /// 授权用户的UID，本字段只是为了方便开发者，减少一次user/show接口调用而返回的，第三方应用不能用此字段作为用户登录状态的识别，只有access_token才是用户授权的唯一票据。
         /// </summary>
         [JsonPropertyName("uid")]
-        public string UserId { get; set; }
+        public string UserId { get; set; } = default!;
     }
 }
