54b25cad021b6ccb67c67f73c771e1f95afb022c
h82258652 <842053625@qq.com>
2021-04-13 11:22:51 +0800

    add unit test

---
+++ test/HN.Social.Weibo.Core.Tests/MockAuthorizationProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using HN.Social.Weibo.Authorization;
+using HN.Social.Weibo.Models;
+
+namespace HN.Social.Weibo.Core.Tests
+{
+    public class MockAuthorizationProvider : IAuthorizationProvider
+    {
+        public Task<AuthorizeResult> AuthorizeAsync(Uri authorizeUri, Uri callbackUri)
+        {
+            return Task.FromResult(new AuthorizeResult());
+        }
+    }
+}
---
+++ test/HN.Social.Weibo.Core.Tests/WeiboClientBuilderTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xunit;
+
+namespace HN.Social.Weibo.Core.Tests
+{
+    public class WeiboClientBuilderTests
+    {
+        [Fact]
+        public void Build_NotSetAccessTokenStorage_ThrowException()
+        {
+            // Arrange
+            var builder = new WeiboClientBuilder().UseAuthorizationProvider<MockAuthorizationProvider>();
+
+            // Act
+            var exception = Assert.Throws<WeiboException>(() => builder.Build());
+
+            // Assert
+            Assert.Equal("未配置 AccessToken 存储", exception.Message);
+        }
+
+        [Fact]
+        public void Build_NotSetAuthorizationProvider_ThrowException()
+        {
+            // Arrange
+            var builder = new WeiboClientBuilder();
+
+            // Act
+            var exception = Assert.Throws<WeiboException>(() => builder.Build());
+
+            // Assert
+            Assert.Equal("未配置 AuthorizationProvider", exception.Message);
+        }
+
+        [Fact]
+        public void Service_NotNull()
+        {
+            // Arrange
+            var builder = new WeiboClientBuilder();
+
+            // Act
+            var services = builder.Services;
+
+            // Assert
+            Assert.NotNull(services);
+        }
+    }
+}
